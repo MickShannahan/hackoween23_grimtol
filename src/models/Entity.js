ae7c797e5e37bc2266dd5b7a5fa35702c15f5c40
jakeoverall <jakeoverall@users.noreply.github.com>
2023-10-27 18:50:51 +0000

    deploy: 087c39c1c99d6320d7c074c2bae5c057b3a91424

--- src/models/Entity.js
+++
@@ -1,9 +0,0 @@
-import { generateId } from "../utils/GenerateId.js"
-
-export class Entity{
-constructor(destroyed = false, hidden = false){
-  this.id = generateId()
-  this.destroyed = destroyed
-  this.hidden = hidden
-}
-}
--- src/models/Item.js
+++
@@ -1,9 +0,0 @@
-import { Entity } from "./Entity.js";
-
-export class Item extends Entity{
-  constructor(data){
-    super(data.destroyed, data.hidden)
-    this.name = data.name
-    this.description = data.description
-  }
-}
