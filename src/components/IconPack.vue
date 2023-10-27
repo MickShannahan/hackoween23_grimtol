4a35985e11bb4261e78a5b0ad801b82c46ac5509
Jake Overall <joverall22@gmail.com>
2023-10-26 23:49:08 -0600

    adds icon pack

---
+++ src/components/IconPack.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="icon-pack">
+    <div class="fs1">
+      <div>
+        <span class="icon-pointer"></span>
+        <span class="mls"> icon-pointer</span>
+      </div>
+      <div>
+        <span class="icon-pointer-alt"></span>
+        <span class="mls"> icon-pointer-alt</span>
+      </div>
+      <div>
+        <span class="icon-bat"></span>
+        <span class="mls"> icon-bat</span>
+      </div>
+      <div>
+        <span class="icon-book"></span>
+        <span class="mls"> icon-book</span>
+      </div>
+      <div>
+        <span class="icon-books"></span>
+        <span class="mls"> icon-books</span>
+      </div>
+      <div>
+        <span class="icon-close"></span>
+        <span class="mls"> icon-close</span>
+      </div>
+      <div>
+        <span class="icon-cog"></span>
+        <span class="mls"> icon-cog</span>
+      </div>
+      <div>
+        <span class="icon-cogs"></span>
+        <span class="mls"> icon-cogs</span>
+      </div>
+      <div>
+        <span class="icon-compass"></span>
+        <span class="mls"> icon-compass</span>
+      </div>
+      <div>
+        <span class="icon-grave"></span>
+        <span class="mls"> icon-grave</span>
+      </div>
+      <div>
+        <span class="icon-grimtol"></span>
+        <span class="mls"> icon-grimtol</span>
+      </div>
+      <div>
+        <span class="icon-grimtol-alt"></span>
+        <span class="mls"> icon-grimtol-alt</span>
+      </div>
+      <div>
+        <span class="icon-hand"></span>
+        <span class="mls"> icon-hand</span>
+      </div>
+      <div>
+        <span class="icon-hand-alt"></span>
+        <span class="mls"> icon-hand-alt</span>
+      </div>
+      <div>
+        <span class="icon-heart"></span>
+        <span class="mls"> icon-heart</span>
+      </div>
+      <div>
+        <span class="icon-knife"></span>
+        <span class="mls"> icon-knife</span>
+      </div>
+      <div>
+        <span class="icon-logo"></span>
+        <span class="mls"> icon-logo</span>
+      </div>
+      <div>
+        <span class="icon-logo-alt"></span>
+        <span class="mls"> icon-logo-alt</span>
+      </div>
+      <div>
+        <span class="icon-mail"></span>
+        <span class="mls"> icon-mail</span>
+      </div>
+      <div>
+        <span class="icon-marker"></span>
+        <span class="mls"> icon-marker</span>
+      </div>
+      <div>
+        <span class="icon-moon"></span>
+        <span class="mls"> icon-moon</span>
+      </div>
+      <div>
+        <span class="icon-paper"></span>
+        <span class="mls"> icon-paper</span>
+      </div>
+      <div>
+        <span class="icon-paper_1"></span>
+        <span class="mls"> icon-paper_1</span>
+      </div>
+      <div>
+        <span class="icon-pentagram"></span>
+        <span class="mls"> icon-pentagram</span>
+      </div>
+      <div>
+        <span class="icon-picture"></span>
+        <span class="mls"> icon-picture</span>
+      </div>
+      <div>
+        <span class="icon-potion"></span>
+        <span class="mls"> icon-potion</span>
+      </div>
+      <div>
+        <span class="icon-skull"></span>
+        <span class="mls"> icon-skull</span>
+      </div>
+      <div>
+        <span class="icon-smile"></span>
+        <span class="mls"> icon-smile</span>
+      </div>
+      <div>
+        <span class="icon-star"></span>
+        <span class="mls"> icon-star</span>
+      </div>
+      <div>
+        <span class="icon-time"></span>
+        <span class="mls"> icon-time</span>
+      </div>
+      <div>
+        <span class="icon-web"></span>
+        <span class="mls"> icon-web</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped></style>
